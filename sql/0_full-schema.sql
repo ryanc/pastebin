488f318f51c50e12ce4cc4ddd4b04c1e90f9ac4b
Ryan Cavicchioni <ryan@confabulator.net>
2012-08-21 18:21:30 -0500

    Removed SQL scripts since switching to Doctrine migrations.

--- sql/0_full-schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE pastes (
-    id INTEGER PRIMARY KEY,
-    timestamp datetime DEFAULT CURRENT_TIMESTAMP,
-    paste TEXT,
-    token VARCHAR(50),
-	filename VARCHAR(100),
-	preview TEXT,
-	ip BINARY(16)
-);
--- sql/2_added-ip-address-column.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE pastes ADD COLUMN ip BINARY(16);
