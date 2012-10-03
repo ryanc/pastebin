ff81b892dddf5e84d63e10525ddee82d73e4ac58
Ryan Cavicchioni <ryan@confabulator.net>
2012-10-02 23:39:52 -0500

    Made the schema.sql script prettier and added more constraints.

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,3 +1,15 @@
-CREATE TABLE doctrine_migration_versions (version VARCHAR(255) NOT NULL, PRIMARY KEY("version"));
-CREATE TABLE paste_content (id INTEGER PRIMARY KEY NOT NULL, content TEXT NOT NULL, digest CHAR(32));
-CREATE TABLE pastes (id INTEGER PRIMARY KEY NOT NULL, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, token VARCHAR(50), filename VARCHAR(100), ip BLOB(16), content_id INTEGER, FOREIGN KEY(content_id) REFERENCES paste_content(id));
+CREATE TABLE pastes (
+    id INTEGER PRIMARY KEY NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    token VARCHAR(50),
+    filename VARCHAR(100),
+    ip BLOB(16) NOT NULL,
+    content_id INTEGER NOT NULL,
+    FOREIGN KEY(content_id) REFERENCES paste_content(id)
+);
+
+CREATE TABLE paste_content (
+    id INTEGER PRIMARY KEY NOT NULL,
+    content TEXT NOT NULL,
+    digest CHAR(32) NOT NULL
+);
