7bbfe402567a0406f9c32313811b2e9f7afed1de
Ryan Cavicchioni <ryan@confabulator.net>
2012-10-06 11:08:57 -0500

    Added unit test for the storage class.

---
+++ tests/StorageTest.php
@@ -0,0 +1,65 @@
+<?php
+
+use Paste\Entity;
+use Paste\Storage;
+
+class StorageTest extends PHPUnit_Framework_TestCase
+{
+    private $path;
+
+    private $db;
+
+    private $app;
+
+    public function setUp()
+    {
+        $app = require __DIR__ . '/../src/app.php';
+        require __DIR__ . '/../src/bootstrap.php';
+
+        $this->app = $app;
+
+        $app['db.options'] = array(
+            'driver' => 'pdo_sqlite',
+            'path'   => ':memory:',
+        );
+
+        $schema = file_get_contents(__DIR__ . "/../sql/schema.sql");
+
+        $this->db = $app['db'];
+        $this->db->exec($schema);
+    }
+
+    public function tearDown()
+    {
+        $this->db->close();
+    }
+
+    public function testPasteSave()
+    {
+        $paste = new Entity\Paste;
+        $paste->setContent('This is a test.');
+        $paste->setTimestamp(time());
+        $paste->setFilename('test.txt');
+        $paste->setIp('127.0.0.1');
+
+        $storage = $this->app['storage'];
+        $id = $storage->save($paste);
+
+        $this->assertNotSame(false, $id);
+
+        $dbPaste = $storage->get($id);
+        $this->assertEquals($dbPaste->getContent(), $paste->getContent());
+        $this->assertEquals($dbPaste->getFilename(), $paste->getFilename());
+        $this->assertEquals($dbPaste->getDigest(), $paste->getDigest());
+        // $this->assertEquals($dbPaste->getIp(), $paste->getIp());
+        // $this->assertEquals($dbPaste->getTimestamp(), $paste->getTimestamp());
+    }
+
+    public function testGetReturnsFalseIfNotInDatabase()
+    {
+        $storage = $this->app['storage'];
+        $paste = $storage->get(99999);
+
+        $this->assertFalse($paste);
+    }
+}
