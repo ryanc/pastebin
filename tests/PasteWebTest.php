62ef264609131fb93e275a7bbddac9a007b24015
Ryan Cavicchioni <ryan@confabulator.net>
2012-10-08 09:58:50 -0500

    Make a temporary folder for all unit test filesystem output.

--- tests/PasteWebTest.php
+++ tests/PasteWebTest.php
@@ -7,22 +7,31 @@ use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class AppTest extends WebTestCase
 {
+    private static $tempDir;
+
+    public static function setUpBeforeClass()
+    {
+        self::$tempDir = exec('mktemp -d');
+    }
+
     public function createApplication()
     {
         $app = require __DIR__ . '/../src/app.php';
         $app['debug'] = true;
         unset($app['exception_handler']);
 
-        $app['monolog.logfile'] = '/tmp/pastebin.log';
+        $tempDir = self::$tempDir;
+
+        $app['monolog.logfile'] = $tempDir . '/pastebin.log';
 
         $app['session.test'] = true;
 
         $app['db.options'] = array(
             'driver' => 'pdo_sqlite',
-            'path'   => '/tmp/pastebin.db',
+            'path'   => $tempDir . '/pastebin.db',
         );
 
-        $app['twig.options'] = array('cache' => __DIR__ . '/tmp/twig');
+        $app['twig.options'] = array('cache' => $tempDir . '/twig');
 
         $app['db']->exec(file_get_contents(__DIR__ . '/../sql/schema.sql'));
 
@@ -35,7 +44,7 @@ class AppTest extends WebTestCase
 
     public function tearDown()
     {
-        @unlink('/tmp/pastebin.db');
+        @unlink(self::$tempDir . '/pastebin.db');
     }
 
     public function testInitialPage()
