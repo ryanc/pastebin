aa88d88c6da8e1083cc85e59b8d2d0d31150d87c
Ryan Cavicchioni <ryan@confabulator.net>
2012-10-14 14:41:04 -0500

    Migration rollback uses less SQL queries now.

--- sql/migrations.sql
+++ sql/migrations.sql
@@ -1,4 +1,4 @@
--- Doctrine Migration File Generated on 2012-10-14 14:10:25
+-- Doctrine Migration File Generated on 2012-10-14 14:10:10
 -- Migrating from 0 to 20121013121601
 
 -- Version 20120820212620
@@ -11,12 +11,10 @@ ALTER TABLE pastes ADD COLUMN ip BLOB(16);
 CREATE TABLE paste_content (id INTEGER PRIMARY KEY NOT NULL, content TEXT NOT NULL, digest CHAR(32));
 ALTER TABLE pastes ADD COLUMN content_id INTEGER;
 INSERT INTO paste_content (content) SELECT paste FROM pastes GROUP BY paste;
-CREATE TEMPORARY TABLE pastes__tmp (id INTEGER PRIMARY KEY NOT NULL, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, token VARCHAR(50), filename VARCHAR(100), ip BLOB(16), content_id INTEGER, FOREIGN KEY(content_id) REFERENCES paste_content(id));
+CREATE TABLE pastes__tmp (id INTEGER PRIMARY KEY NOT NULL, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, token VARCHAR(50), filename VARCHAR(100), ip BLOB(16), content_id INTEGER, FOREIGN KEY(content_id) REFERENCES paste_content(id));
 INSERT INTO pastes__tmp SELECT p.id, p.timestamp, p.token, p.filename, p.ip, c.id FROM pastes p, paste_content c WHERE c.content = p.paste;
 DROP TABLE pastes;
-CREATE TABLE pastes (id INTEGER PRIMARY KEY NOT NULL, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, token VARCHAR(50), filename VARCHAR(100), ip BLOB(16), content_id INTEGER, FOREIGN KEY(content_id) REFERENCES paste_content(id));
-INSERT INTO pastes SELECT * FROM pastes__tmp;
-DROP TABLE pastes__tmp;
+ALTER TABLE pastes__tmp RENAME TO pastes;
 
 -- Version 20121013121601
 ALTER TABLE pastes ADD COLUMN highlight BOOLEAN DEFAULT 1;
